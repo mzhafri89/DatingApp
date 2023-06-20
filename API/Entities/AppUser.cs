097c852422ae69ce88d7ec7305de174b748d876f
zaf <mzhafri89@gmail.com>
2023-06-20 10:41:59 +0800

    entity framework and database migrations

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,8 @@ namespace API.Entities
 {
     public class AppUser
     {
+        // * Class properties needs to follow the convention of the entity framework
+        // * Id is the primary key  of the table
         public int Id { get; set; }
 
         // * Dot net 6+ allows for nullables string, can be disabled in API.csproj
--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,19 @@
 // * Main entry point of dotnet run command
 
 // * Create web application instance
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // * Add services to the container.
 builder.Services.AddControllers();
+
+// * Database connection
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
