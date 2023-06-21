6ab10e7e39fddbdd9b130653df487c4431467ffe
Mohd Zhafri Zulkafli <mzhafri89@gmail.com>
2023-06-21 12:20:00 +0800

    Skeleton UI (#2)
    * angular cli project generation
    
    * Add cors header.
    
    * Fetchs Users.
    
    * misc
    
    * ssl and ngx-bootstrap setup
    
    * reformat
    
    * run client on https

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface User {
+  // interface for type safety
+  userName: string;
+  id: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'client';
+  users: User[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: (response) => {
+        this.users = response as User[];
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
+}
