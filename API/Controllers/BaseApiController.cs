4d4cc55ced1a2c6498fe3b0bbde36961a2b24c54
zaf <mzhafri89@gmail.com>
2023-06-22 11:40:17 +0800

    refactor

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    // * ApiController allows for automatic DTO binding and validation
     [ApiController]
     [Route("api/[controller]")]
     // * Parent class for all API controllers
