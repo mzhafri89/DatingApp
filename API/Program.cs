ff3b161f6496d5daf8dce999ff6a9c5d589e330f
zaf <mzhafri89@gmail.com>
2023-06-22 13:09:22 +0800

    Token middleware.

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,9 @@ using API.Data;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,12 +38,34 @@ builder.Services.AddCors(opt =>
 // * transient - service is created each time it is requested
 builder.Services.AddScoped<ITokenService, TokenService>();
 
+// * Add authentication middleware
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])
+            ),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
 var app = builder.Build();
 
 // * Middlewares section
 
 // * Use cors policy
 app.UseCors("CorsPolicy");
+
+// * Use authentication middleware - validates the token
+app.UseAuthentication();
+
+// * Use authorization middleware - checks if user is authorized to access the resource
+app.UseAuthorization();
 
 app.MapControllers();
 
