36dd1d2e771313958c2decb211c5c7cecc43c37a
zaf <mzhafri89@gmail.com>
2023-06-22 21:06:15 +0800

    login form

--- client/src/app/core/modules/auth/auth.module.ts
+++ client/src/app/core/modules/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthLandingComponent } from './pages/auth-landing/auth-landing.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AuthLandingComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: AuthLandingComponent }]),
   ],
 })
--- client/src/app/core/modules/auth/pages/auth-landing/auth-landing.component.ts
+++ client/src/app/core/modules/auth/pages/auth-landing/auth-landing.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-landing',
   templateUrl: './auth-landing.component.html',
-  styleUrls: ['./auth-landing.component.css']
+  styleUrls: ['./auth-landing.component.css'],
 })
-export class AuthLandingComponent {
+export class AuthLandingComponent implements OnInit {
+  //@ts-ignore
+  form: FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+  }>;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.form = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
