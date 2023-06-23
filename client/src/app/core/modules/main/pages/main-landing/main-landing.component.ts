8d0854d29c044e4211af09f349be738236f026d7
zaf <mzhafri89@gmail.com>
2023-06-23 08:52:10 +0800

    auto redirect to list after login

--- client/src/app/core/modules/main/pages/main-landing/main-landing.component.ts
+++ client/src/app/core/modules/main/pages/main-landing/main-landing.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-landing',
   templateUrl: './main-landing.component.html',
-  styleUrls: ['./main-landing.component.css']
+  styleUrls: ['./main-landing.component.css'],
 })
-export class MainLandingComponent {
+export class MainLandingComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
+  ngOnInit(): void {
+    this.router.navigate(['list'], { relativeTo: this.route });
+  }
 }
