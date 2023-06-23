38b10e327aa14f3bb5036136706079ea568b8998
zaf <mzhafri89@gmail.com>
2023-06-23 18:21:00 +0800

    import http modules to needed modules only.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    HttpClientModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     RouterModule,
--- client/src/app/core/modules/auth/auth.module.ts
+++ client/src/app/core/modules/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { AuthLandingComponent } from './pages/auth-landing/auth-landing.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
 
 import authReducer from './store/auth.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './store/auth.effects';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthLandingComponent } from './pages/auth-landing/auth-landing.component';
 import { AuthTokenInterceptor } from './interceptors/auth-token.interceptor';
 
 @NgModule({
@@ -16,6 +16,7 @@ import { AuthTokenInterceptor } from './interceptors/auth-token.interceptor';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    HttpClientModule,
     StoreModule.forFeature('auth', authReducer),
     EffectsModule.forFeature([AuthEffects]),
     RouterModule.forChild([{ path: '', component: AuthLandingComponent }]),
