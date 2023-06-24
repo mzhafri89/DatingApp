cc2c655ca6d20df79c31feca37daa68c843e6d88
zaf <mzhafri89@gmail.com>
2023-06-24 22:50:26 +0800

    specify user service get users response value.

--- client/src/app/share/services/users.service.ts
+++ client/src/app/share/services/users.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +11,6 @@ export class UsersService {
   constructor(private http: HttpClient) {}
 
   getUsers() {
-    return this.http.get(`${environment.apiUrl}/users`);
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
   }
 }
