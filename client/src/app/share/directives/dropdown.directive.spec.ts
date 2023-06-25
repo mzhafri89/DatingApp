3bd4f2ae7730cb9e5016d919103c8694a330f513
zaf <mzhafri89@gmail.com>
2023-06-25 10:07:52 +0800

    unit test

--- client/src/app/share/directives/dropdown.directive.spec.ts
+++ client/src/app/share/directives/dropdown.directive.spec.ts
@@ -1,8 +1,54 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { DropdownDirective } from './dropdown.directive';
 
+@Component({
+  template: `
+    <div class="dropdown" dropdown>
+      <button class="btn btn-secondary dropdown-toggle" type="button">
+        Welcome User
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Edit Profile</a></li>
+        <li><a class="dropdown-item" href="#" (click)="logout()">Logout</a></li>
+      </ul>
+    </div>
+  `,
+})
+class TestComponent {}
+
 describe('DropdownDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let dropdownEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DropdownDirective, TestComponent],
+    });
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    dropdownEl = fixture.debugElement.query(By.directive(DropdownDirective));
+  });
+
   it('should create an instance', () => {
-    const directive = new DropdownDirective();
+    const directive = dropdownEl.injector.get(DropdownDirective);
     expect(directive).toBeTruthy();
+  });
+
+  it('should toggle the dropdown menu when the host element is clicked', () => {
+    const buttonEl = dropdownEl.query(By.css('.dropdown-toggle')).nativeElement;
+    const dropdownMenuEl = dropdownEl.query(
+      By.css('.dropdown-menu')
+    ).nativeElement;
+
+    buttonEl.click();
+    fixture.detectChanges();
+    expect(dropdownMenuEl.classList).toContain('show');
+
+    buttonEl.click();
+    fixture.detectChanges();
+    expect(dropdownMenuEl.classList).not.toContain('show');
   });
 });
--- client/src/app/share/services/users.service.spec.ts
+++ client/src/app/share/services/users.service.spec.ts
@@ -1,16 +1,42 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user.interface';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService],
+    });
     service = TestBed.inject(UsersService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should retrieve users from the API via GET', () => {
+    const mockUsers: User[] = [
+      { username: 'John Doe' },
+      { username: 'Jane Doe' },
+    ];
+
+    service.getUsers().subscribe((users) => {
+      expect(users).toEqual(mockUsers);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/users`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
   });
 });
