f6fda9032beffe016e818de848bd34b905a52f71
zaf <mzhafri89@gmail.com>
2023-06-26 12:53:23 +0800

    Exception middleware.

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,63 @@
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private RequestDelegate next;
+        private ILogger<ExceptionMiddleware> logger;
+        private IHostEnvironment env;
+
+        public ExceptionMiddleware(
+            RequestDelegate next,
+            ILogger<ExceptionMiddleware> logger,
+            IHostEnvironment env
+        )
+        {
+            this.next = next;
+            this.logger = logger;
+            this.env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                // * If no exception is thrown, continue to the next middleware
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                // * Log the exception
+                logger.LogError(ex, ex.Message);
+
+                // * Set the response status code
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                // * Set the response content type
+                context.Response.ContentType = "application/json";
+
+                // * Get the response body
+                var response = env.IsDevelopment()
+                    ? new APIException(
+                        context.Response.StatusCode,
+                        ex.Message,
+                        ex.StackTrace?.ToString()
+                    )
+                    : new APIException(context.Response.StatusCode, "Internal Server Error", null);
+
+                // * Serialize the response body
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                // * Write the response body to the response
+                var json = JsonSerializer.Serialize(response, options);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
